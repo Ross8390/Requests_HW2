3469a370b889cda9ae4dec3dd67fd023e2c173ad
“Aleksandr <ross1990@mail.ru>
2022-12-10 23:31:31 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+
+TOKEN = y0_AgAAAAAbkEYvAADLWwAAAADWZXm2CQahUdcnSB2CFsT5MjPEHCHCf9c
+
+class YaUploader:
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authoriztion': f'OAuth {self.token}'
+        }
+
+    def get_files_list(self):
+        files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
+        headers = self.get_headers()
+
+    def upload(self, file_path: str):
+        """Метод загружает файлы по списку file_list на яндекс диск"""
+        # Тут ваша логика
+        # Функция может ничего не возвращать
+
+
+if __name__ == '__main__':
+    # Получить путь к загружаемому файлу и токен от пользователя
+    path_to_file = ...
+    token = ...
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file)
+
