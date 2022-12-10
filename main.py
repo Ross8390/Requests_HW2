dfe223746824e8767a76a2d11fb8d82cb635400c
“Aleksandr <ross1990@mail.ru>
2022-12-11 00:33:53 +0300

    before review

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 import requests
 
-TOKEN = y0_AgAAAAAbkEYvAADLWwAAAADWZXm2CQahUdcnSB2CFsT5MjPEHCHCf9c
+TOKEN = ''
 
-class YaUploader:
+class YandexDisk:
     def __init__(self, token):
         self.token = token
 
     def get_headers(self):
         return {
             'Content-Type': 'application/json',
-            'Authoriztion': f'OAuth {self.token}'
+            'Authorization': f'OAuth {self.token}'
         }
 
     def get_files_list(self):
         files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
         headers = self.get_headers()
+        response = requests.get(files_url, headers=headers)
+        return response.json()
 
-    def upload(self, file_path: str):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-        # Функция может ничего не возвращать
+    def get_upload_link(self, disk_file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        params = {'path': disk_file_path}
+        response = requests.get(upload_url, headers=headers, params=params)
+        print(response.json())
+        return response.json()
+
+    def upload_file_to_disk(self, disk_file_path, filename):
+        href = self.get_upload_link(disk_file_path=disk_file_path).get('href','')
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print('Success')
 
 
 if __name__ == '__main__':
-    # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = ...
-    token = ...
-    uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
+   ya = YandexDisk(token=TOKEN)
+   print(ya.get_files_list())
+   print(ya.upload_file_to_disk('test1.txt','test.txt'))
 
